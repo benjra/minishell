60ef3bd5863ff6ea145803f6c6160482fcad9642
amabchou <amabchou@c3r1p3.1337.ma>
2024-07-09 21:12:03 +0100

    p

--- execution/emain.c
+++ execution/emain.c
@@ -2,10 +2,14 @@
 
 int ft_pwd(void);
 void   ft_echo(char *str, int n);
+void    ft_exit(void);
+
 
 int main(int ac, char **av, char **env) {
     
     //ft_pwd();
     ft_echo("Hellooooooooooooo", 0);
+    ft_exit();
+    ft_echo("Byyyyyyyyyyyyyyyyyyye", 0);
     return 0;
 }
--- execution/mini_exit.c
+++ execution/mini_exit.c
@@ -6,8 +6,13 @@
 /*   By: amabchou <amabchou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 20:56:18 by amabchou          #+#    #+#             */
-/*   Updated: 2024/07/09 20:56:43 by amabchou         ###   ########.fr       */
+/*   Updated: 2024/07/09 21:10:18 by amabchou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parsing/mini.h"
+
+void    ft_exit(void)
+{
+    exit(0);
+}
